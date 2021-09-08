07ffcafc4a9b59e1299ebeb105e3c4d40a0335d7
DistractedBranches <damolajao@gmail.com>
2021-09-08 14:18:22 -0400

    Progress

---
+++ RevProj0/src/main/java/com/revature/models/Playlist.java
@@ -0,0 +1,56 @@
+package com.revature.models;
+
+public class Playlist {
+    private Integer id;
+    private String title;
+    private User user;
+    private Song song;
+
+    public Playlist(Integer id, String title, Song song) {
+        this.id = id;
+        this.title = title;
+        this.song = song;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Song getSong() {
+        return song;
+    }
+
+    public void setSong(Song song) {
+        this.song = song;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Playlist{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", user=" + user +
+                ", song=" + song +
+                '}';
+    }
+}
---
+++ RevProj0/src/main/java/com/revature/models/Song.java
@@ -0,0 +1,68 @@
+package com.revature.models;
+
+public class Song {
+    private Integer id;
+    private String title;
+    private String artist;
+    private String genre;
+    private String releaseDate;
+
+    public Song(Integer id, String title, String artist, String genre, String releaseDate) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.releaseDate = releaseDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Song{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", artist='" + artist + '\'' +
+                ", genre='" + genre + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                '}';
+    }
+}
